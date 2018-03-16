429f78afaae6295a7a687715fb9ea9ce9e7021ff
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-03-15 22:32:05 -0700

    game working up to showing products for sale, and asking user which prod and how many they would like to buy. next step is updating db with new quantity after purchase

---
+++ app.js
@@ -0,0 +1,99 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+//create a connection to mysql database
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: "",
+    database: 'bamazon_db'
+})
+connection.connect(function (err) {
+    console.log(`Connected as id: ${connection.threadId}`);
+})
+
+
+var displayItems  = function() {
+    
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        //console.log(res);
+        var availableProducts = [];
+        res.forEach((element, index) => {
+            console.log("------------------");
+            console.log("FOR SALE:")
+            console.log(`ItemID:${res[index].id}, Description: ${res[index].productName},  Price: ${res[index].price}`);
+            availableProducts.push(res[index].productName)
+            //console.log(availableProducts);
+        });
+        inquirer.prompt([
+            {
+                name: 'item',
+                message: 'What item would you like to buy today?',
+                type: 'list',
+                choices: availableProducts
+            }
+        ]).then(function(answer){
+            console.log(`You have chosen to purchase ${answer.item}!`);
+            
+            for (j=0; j<availableProducts.length; j++) {
+                debugger;
+                if (answer.item === res[j].productName) {
+                    var chosenItem = res[j];
+                } 
+            };
+            inquirer.prompt([
+                {
+                    name: 'quantity',
+                    message: 'How many units would you like to buy?',
+                    type: 'input',
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        } else {
+                            console.log("  !!Please input a valid # for the quantity of items to buy!!");
+                            return false;
+                        }
+                    }
+                }
+            ]).then(function (answer){
+                //check if quantity is available
+                console.log(chosenItem);
+                if (parseInt(answer.quantity) < chosenItem.quantity ) {
+                    console.log("The product is in stock!");
+                    var newQuantity = parseInt(chosenItem.quantity) - parseInt(answer.quantity);
+                    //UPDATE IN DB
+                    var query = connection.query(
+                        "UPDATE products SET ? WHERE ?", [
+                            {
+                                //new quantity of selected item
+                                quantity: newQuantity
+                            },
+                            {
+                                //location of item to be updated
+                                id: chosenItem.id
+                            }
+                        ]
+                    )
+
+                } else {
+                    console.log('sorry the item is out of stock :( ');
+                }
+            })
+        })
+        connection.end();
+    });
+    //whatToBuy(res);
+}
+
+//   function whatToBuy(res) {
+//     inquirer.prompt([
+//         {
+//             name: 'choice',
+//             message: 'What item would you like to buy today?',
+//             type: 'list',
+//             choices: 
+//         }
+//     ])
+//   };
+displayItems();
